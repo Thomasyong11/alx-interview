f89b9a1477c66fb0d6c23b9b3173c12cd9a880db
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-11-14 09:45:00 +0000

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,59 +1,17 @@
 #!/usr/bin/python3
-"""_summary_
-"""
-
-
-def transpose_matrix(matrix, n):
-    """_summary_
-
-    Args:
-                    matrix (_type_): _description_
-    """
-    for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
-
-def reverse_matrix(matrix):
-    """_summary_
-
-    Args:
-                    matrix (_type_): _description_
-    """
-    for row in matrix:
-        row.reverse()
+''' Rotate 2D Matrix '''
 
 
 def rotate_2d_matrix(matrix):
-    """_summary_
-
-    Args:
-                    matrix (_type_): _description_
-    """
-    n = len(matrix)
-    # print(n)
-
-    """sample matrix
-    1 2 3
-    4 5 6
-    7 8 9
-    """
-
-    # transpose matrix
-    """
-    1 4 7
-    2 5 8
-    3 6 9
-    """
-
-    transpose_matrix(matrix, n)
-
-    # reverse matrix
-    """
-    7 4 1
-    8 5 2
-    9 6 3
-    """
-    reverse_matrix(matrix)
-
-    return matrix
+    ''' rotate a given n x n 2d matrix by 90 degrees clockwise. '''
+    rows = len(matrix)
+    for row in range(int(rows / 2)):
+        offset = 0
+        i = rows - 1 - row
+        for col in range(row, i):
+            tmp = matrix[row][col]
+            matrix[row][col] = matrix[i - offset][row]
+            matrix[i - offset][row] = matrix[i][i - offset]
+            matrix[i][i - offset] = matrix[col][i]
+            matrix[col][i] = tmp
+            offset += 1
